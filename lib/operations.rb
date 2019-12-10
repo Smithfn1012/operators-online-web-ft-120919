b52a34720838848e276a9cac8b9c9298de8b62a7
Felicia Smith <smithfn1012@gmail.com>
2019-12-10 00:59:51 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,16 @@
 def unsafe?(speed)
-
+  if speed < 40
+    return "You are going too slow!"
+  elseif speed > 60
+    return "You are going too fast!"
+  else
+    return "You are driving a safe speed"
 end
 
 
 
 def not_safe?(speed)
-	
+	speed ? unsafe : safe
 end
 	
 
